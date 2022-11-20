8ae06f5859a70681d7676fc514707a9bf4e07c7a
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-11-20 21:46:15 +0800

    Header.vue was created and added to App.vue

--- recordstore-frontend/src/App.vue
+++ recordstore-frontend/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <Header/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue' /* @ REFERS TO SRC FOLDER IN recordstore-frontend */
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
